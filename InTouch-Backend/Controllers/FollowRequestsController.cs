21c1c6779c7c5e9abfbaf32881067770a21b624d
Aldrin1807 <ai58914@ubt-uni.net>
2023-05-10 13:17:45 +0200

    Added Controller class with service injection

---
+++ InTouch-Backend/Controllers/FollowRequestsController.cs
@@ -0,0 +1,15 @@
+﻿using InTouch_Backend.Data.Services;
+
+namespace InTouch_Backend.Controllers
+{
+    public class FollowRequestsController
+    {
+        private readonly FollowRequestsService _service;
+
+        public FollowRequestsController(FollowRequestsService service)
+        {
+            _service = service;
+        }
+
+    }
+}
