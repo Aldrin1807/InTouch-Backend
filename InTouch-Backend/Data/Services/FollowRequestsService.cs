e98f3e1283c581a07bbc41e5feca38ecde4b6be6
Aldrin1807 <ai58914@ubt-uni.net>
2023-05-10 13:18:14 +0200

    Added methods on service for requesting and unrequesting follow

--- InTouch-Backend/Data/Services/FollowRequestsService.cs
+++ InTouch-Backend/Data/Services/FollowRequestsService.cs
@@ -1,4 +1,7 @@
-﻿namespace InTouch_Backend.Data.Services
+﻿using InTouch_Backend.Data.DTOs;
+using InTouch_Backend.Data.Models;
+
+namespace InTouch_Backend.Data.Services
 {
     public class FollowRequestsService
     {
@@ -7,6 +10,29 @@
         public FollowRequestsService(AppDbContext context)
         {
             _context = context; 
+        }
+
+        public void requestFollow(FollowRequestsDTO request)
+        {
+            var _request = new FollowRequests()
+            {
+                FollowRequestId = request.FollowRequestId,
+                FollowRequestedId = request.FollowRequestedId
+            };
+            _context.Add(_request);
+            _context.SaveChanges();
+        }
+
+        public void unRequestFollow(FollowRequestsDTO request)
+        {
+            var _request = _context.FollowRequests.FirstOrDefault(f => f.FollowRequestId == request.FollowRequestId && f.FollowRequestedId == request.FollowRequestedId);
+
+            if (_request != null)
+            {
+                _context.Remove(_request);
+                _context.SaveChanges();
+            }
+
         }
 
     }
