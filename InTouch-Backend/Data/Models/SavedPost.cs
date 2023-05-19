c5b93584980ceb34f97aec4095ec4235b8359bd8
Aid Maliqi <aid.maliqi04@gmail.com>
2023-05-19 10:22:28 +0200

    added savedpost model

---
+++ InTouch-Backend/Data/Models/SavedPost.cs
@@ -0,0 +1,12 @@
+﻿namespace InTouch_Backend.Data.Models
+{
+    public class SavedPost
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
