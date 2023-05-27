86fbefe7156a57770b0bd957a843eef774fa09ce
Aid Maliqi <aid.maliqi04@gmail.com>
2023-05-28 00:18:05 +0200

    created support message contoller

---
+++ InTouch-Backend/Controllers/SupportMessagesController.cs
@@ -0,0 +1,48 @@
+﻿using InTouch_Backend.Data.DTOs;
+using InTouch_Backend.Data.Models;
+using InTouch_Backend.Data.Services;
+using InTouch_Backend.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InTouch_Backend.Controllers
+{
+    [Route("api/[controller]")]
+        [ApiController]
+    public class SupportMessagesController : ControllerBase
+    {
+        
+        
+        
+            private readonly SupportMessageService _supportMessage;
+
+            public SupportMessagesController(SupportMessageService supportMessage)
+            {
+            _supportMessage = supportMessage;
+            }
+
+            
+
+            [HttpGet("get-support-messages")]
+            public IActionResult getSupportMessages()
+            {
+            return Ok(_supportMessage.getSupportMessages());
+            }
+
+            [HttpDelete("delete-support-message")]
+            public IActionResult deleteReport([FromBody] DeleteSupportMessagesDTO messages)
+            {
+                try
+                {
+                    _supportMessage.deleteSupportMessage(messages);
+                    return Ok(new Response
+                    { Status = "Success", Message = "Message deleted succesfully." });
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new Response
+                    { Status = "Error", Message = ex.Message });
+                }
+            }
+        }
+    
+}
